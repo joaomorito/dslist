b6557893327d4f39d4f645532c4e80b4cf3ffbdd
joaomorito <joaomorito04@gmail.com>
2024-11-11 11:27:56 -0300

    Project created

---
+++ src/test/java/com/joaomorito/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomorito.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
