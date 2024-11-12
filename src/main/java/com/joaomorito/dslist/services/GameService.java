b247ee2811fb394c18653ef32de6147b18b4ffcb
joaomorito <joaomorito04@gmail.com>
2024-11-12 14:52:27 -0300

    SQL consult, projection, search games by list

--- src/main/java/com/joaomorito/dslist/services/GameService.java
+++ src/main/java/com/joaomorito/dslist/services/GameService.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.joaomorito.dslist.dto.GameDTO;
 import com.joaomorito.dslist.dto.GameMinDTO;
 import com.joaomorito.dslist.entities.Game;
+import com.joaomorito.dslist.projections.GameMinProjection;
 import com.joaomorito.dslist.repositories.GameRepository;
-
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
@@ -27,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
